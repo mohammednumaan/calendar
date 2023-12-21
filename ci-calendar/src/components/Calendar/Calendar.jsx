1e8050c83adde7f4ed056b82cf5786043163c587
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2023-12-21 19:04:43 +0530

    code : Created basic calendar ui

---
+++ ci-calendar/src/components/Calendar/Calendar.jsx
@@ -0,0 +1,32 @@
+import FullCalendar from "@fullcalendar/react";
+import daygridPlugin from "@fullcalendar/daygrid";
+import timegridPlugin from "@fullcalendar/timegrid";
+import multiMonthPlugin from '@fullcalendar/multimonth'
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from "@fullcalendar/interaction";
+import './Calendar.css'
+
+export default function Calendar(){
+  return (
+    <div className="calendar">
+        <div className="calendar-container">
+
+            <h1 className="calendar-title">Your Calendar </h1>
+            <FullCalendar 
+
+            themeSystem={'bootstrap4'}
+            aspectRatio={2.8}
+            plugins={[daygridPlugin, timegridPlugin, multiMonthPlugin, interactionPlugin, listPlugin]}
+            initialView={"dayGridMonth"}
+            
+            headerToolbar={{start : 'today prev,next', center : 'title', end : 'dayGridMonth,dayGridWeek,timeGridDay,list'}}
+            selectable={true}
+
+        
+            />
+
+        </div>
+      
+    </div>
+  );
+}
